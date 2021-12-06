48bd7f02495163a3821ad0c42ad76f575d438682
Joshua Jolley <jolleyboy@clearwateranalytics.com>
2021-12-06 15:37:47 -0700

    Whitespace cleanup

--- src/main/kotlin/advent/twentytwentyone/puzzles/Day3.kt
+++ src/main/kotlin/advent/twentytwentyone/puzzles/Day3.kt
@@ -26,7 +26,6 @@ class Day3 {
         .map { if (it == '1') '0' else '1' }
         .joinToString("")
 
-
     suspend fun parseInput(): List<String> {
         return getInput(2021, 3)
             .split('\n')
@@ -35,18 +34,18 @@ class Day3 {
 
     suspend fun partTwo(): Int {
         val binaryNumbers = parseInput()
-        val o2Rating = whittleDown(binaryNumbers, 0) {if (it >= 0.5) '1' else '0'}
-        val co2Rating = whittleDown(binaryNumbers, 0) {if (it >= 0.5) '0' else '1'}
+        val o2Rating = whittleDown(binaryNumbers, 0) { if (it >= 0.5) '1' else '0' }
+        val co2Rating = whittleDown(binaryNumbers, 0) { if (it >= 0.5) '0' else '1' }
         return o2Rating.toInt(2) * co2Rating.toInt(2)
     }
 
-    tailrec fun whittleDown(numbers: List<String>, index: Int = 0, searchKyFn: (x: Double) -> Char): String{
+    tailrec fun whittleDown(numbers: List<String>, index: Int = 0, searchKyFn: (x: Double) -> Char): String {
         return if (numbers.size == 1) {
-            numbers.first();
+            numbers.first()
         } else {
             val avg = numbers.map { it[index].toString().toInt() }.average()
             val searchKey = searchKyFn(avg)
-            whittleDown(numbers.filter { it[index] == searchKey}, index + 1, searchKyFn)
+            whittleDown(numbers.filter { it[index] == searchKey }, index + 1, searchKyFn)
         }
     }
 }
